be07e0ad6234433388492888a73e13d5dac9a3dc
priyasri01 <Priyasri.april14@gmail.com>
2022-12-21 12:32:02 -0500

    The basic Solar system with the featuresun and solorsystem added and it's ready to deploy

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+      SolarSystem XX = new SolarSystem();
+
+      XX.moon=2;
+      XX.planets=2;
+      XX.sun=1;
+
+      FeatureSun fxron = new FeatureSun();
+      fxron.color="Green";
+      fxron.size="23456";
+      fxron.temp="-1457413";
+
+    }
+}
